06f68977c88aafc9979477aea8f6a461cdba1c7e
Miko <miko@gmail.com>
2024-12-19 20:05:53 +0500

    header done

---
+++ portfolio/src/layout/Header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  let activePage = ref(1);
+
+  function changeActivePage(page:number){
+    activePage.value = page;
+  }
+</script>
+
+<template>
+  <nav class="flex justify-between items-center py-5">
+    <h1 class="main-text">Miras Baidildin</h1>
+    <div>
+      <a :class="activePage === 1 ? 'active' : ''" @click="changeActivePage(1)">Home</a>
+      <a :class="activePage === 2 ? 'active' : ''" @click="changeActivePage(2)">Front-end skills</a>
+      <a :class="activePage === 3 ? 'active' : ''" @click="changeActivePage(3)">Back-end skills</a>
+      <a :class="activePage === 4 ? 'active' : ''" @click="changeActivePage(4)">Projects</a>
+      <a :class="activePage === 5 ? 'active' : ''" @click="changeActivePage(5)">Contacts</a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  a{
+    margin-right:76px;
+    color: #fff;
+    opacity: 0.5;
+    cursor: pointer;
+  }
+
+ .active {
+    opacity: 1;
+    font-weight: bold;
+  }
+
+</style>
